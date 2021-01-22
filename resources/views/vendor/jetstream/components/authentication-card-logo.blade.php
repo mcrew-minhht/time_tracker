906e9906a4d8d78f701ae404aee830207f65b9ad
minhht <minhht@linktech.co.jp>
2021-01-22 22:33:53 +0700

    Update

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,8 @@
-<a href="/">
-    <img src="{{url('images/logo.png')}}" alt="logo Mcrew-tech" width="165px;"height="auto">
-</a>
+<div align="center">
+    <a href="/">
+        <img src="{{url('images/logo.png')}}" alt="logo Mcrew-tech" width="400px;"height="auto">
+    </a>
+
+    <h1 style="padding-top: 100px">TIME TRACKER</h1>
+</div>
+
